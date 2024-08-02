75a9df7c46037a83319020eb3abe4ec57f0f2ee6
caiobinotti <130671116+caiobinotti@users.noreply.github.com>
2024-08-02 07:45:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const caixaprincipal = document.querySelector('.caixa-principal');
+const caixaPergunta = document.querySelector('caixa-pergunta');
+const caixaLterniva = document.querySelector('caixa-alternaiva');
+const cixareultado = documnt.querySelector('caixaa-resultao');
+
+const perguntas =[//abre uma lista qualquer
+    {//obre o objetivo da lista (cada item)
+        enunciado:"pergunta 1",
+        alternativas: [
+            "alternativa 1",
+            "alternativa 2"
+        ]
+    }//fechar a o objeto da lista (fim do item
+]//fechando a lista toda (completa)
+
